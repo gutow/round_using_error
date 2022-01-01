349f26eaead71c560f5c07f7c892fa921c274640
Jonathan Gutow <gutow@uwosh.edu>
2021-12-31 20:36:46 -0600

    Bump to 1.0.1. Release status to Stable.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="round_using_error",
-    version="1.0.0",
+    version="1.0.1",
     description="Output numbers +/- error with appropriate rounding.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setuptools.setup(
     include_package_data=True,
     install_requires=[],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
